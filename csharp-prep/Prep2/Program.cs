5730d389f3544457e673a02960097b64629b3014
Jaemok Jo <jaemok.jo@gmail.com>
2023-04-25 15:03:08 -0600

    GradeandName

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write ("What is your grade percentage? ");
+        int gradeper = int.Parse(Console.ReadLine());
+
+        if (gradeper>=70)
+        {Console.WriteLine ("You passed it!!");
+
+        if (gradeper>=90)
+        {Console.WriteLine("Your grade is A");}
+        else if (gradeper>=80)
+        {Console.WriteLine("Your grade is B");}
+        else if (gradeper>=70)
+        {Console.WriteLine("Your grade is C");}
+        else if (gradeper>=60)
+        {Console.WriteLine("Your grade is D");}
+        else
+        {Console.WriteLine("Your grade is F");}
+
+        }
+        else
+        {Console.WriteLine("Sorry, You should take this class again.");}
     }
 }
