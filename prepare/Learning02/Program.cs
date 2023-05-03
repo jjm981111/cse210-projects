cdd1b7c0bc7fc1ccf3e6bfd7615d8e1764d96031
Jaemok Jo <jaemok.jo@gmail.com>
2023-05-03 16:00:17 -0600

    JobResume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        var job1 = new Job ("Programmer","Google",2025,2028);
+        var job2 = new Job ("Software Engineer", "Amazon",2028,2040);
+        var myname = new Resume("Jaemok Jo");
+
+        myname.Displayname();
+        Console.WriteLine("Jobs:");
+        job1.Displayjob();
+        job2.Displayjob();
+
+        
     }
+}
+
+public class Job {
+
+    public string _jobtitle;
+    public string _company;
+    public int _startyear;
+    public int _endyear;
+
+    public Job (string jt, string co, int sy, int ey) {
+        _jobtitle = jt;
+        _company = co;
+        _startyear = sy;
+        _endyear = ey;
+    }
+
+    public void Displayjob() {
+        Console.WriteLine($"{_jobtitle} ({_company}) {_startyear}-{_endyear}");
+    }
+
+
+}
+
+
+public class Resume {
+    public string name;
+
+    public Resume (string nm) {
+        name = nm;
+    }
+
+    public void Displayname(){
+        Console.WriteLine ($"Name: {name}");
+    }
+
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!!!!!");
+        //object
+        //instance
+        var layne = new Person("Layne", "Moseley");
+        var david = new Person("David", "Hasselhoff");
+
+    
+
+        layne.Talk();
+        david.Talk();
     }
 }
+
+
+//classification
+class Person {
+
+
+    //attributes
+    //properties
+    string firstName;
+    string lastName;
+
+    //constructor
+    public Person(string fn, string ln) {
+        firstName = fn;
+        lastName = ln;
+    }
+
+    // behaviors
+    public void Breath() {
+        Console.WriteLine("Breathing");
+    }
+
+    public void Walk(){
+        Console.WriteLine("Walking");
+    }
+
+    public void Talk(){
+        Console.WriteLine($"Hi! Myname is {FullName()}");
+    }
+
+    public string FullName() {
+        return $"{firstName} {lastName}";
+    }
+
+}
+
