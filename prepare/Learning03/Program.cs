f23af33a1774e7bfa0a7f59783eac513b9e985e0
Jaemok Jo <jaemok.jo@gmail.com>
2023-05-16 16:08:25 -0600

    fractions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        var fraction1 = new Fraction (1,1);
+        var fraction2 = new Fraction (5,1);
+        var fraction3 = new Fraction (3,4);
+        var fraction4 = new Fraction (1,3);
+
+        fraction1.GetFractionString();
+        fraction1.GetDecimalValue();
+        fraction2.GetFractionString();
+        fraction2.GetDecimalValue();
+        fraction3.GetFractionString();
+        fraction3.GetDecimalValue();
+        fraction4.GetFractionString();
+        fraction4.GetDecimalValue();
+        
+
+
     }
 }
+
+
+
+public class Fraction {
+    public float _top;
+    public float _bottom;
+
+    public Fraction (float top, float bottom){
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void GetFractionString (){
+        Console.WriteLine ($"{_top}/{_bottom}");
+    }
+
+    public void GetDecimalValue () {
+        float decimalfraction = _top/_bottom;
+        Console.WriteLine ($"{decimalfraction}");
+    }
+}
+
