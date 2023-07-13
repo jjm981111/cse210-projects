f97b5cf8a217603b98efd1cb67fb8af4224eb216
Jaemok Jo <jaemok.jo@gmail.com>
2023-07-13 14:32:31 -0600

    Foundation 3,4

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,139 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        var lectureEvent = new LectureEvent("Lecture Title", "Lecture Description", DateTime.Now, TimeSpan.FromHours(2),
+            new Address("123 Main St", "City", "State", "Country"), "John Doe", 50);
+
+        var receptionEvent = new ReceptionEvent("Reception Title", "Reception Description", DateTime.Now, TimeSpan.FromHours(3),
+            new Address("456 Elm St", "City", "State", "Country"), "rsvp@example.com");
+
+        var outdoorGatheringEvent = new OutdoorGatheringEvent("Outdoor Gathering Title", "Outdoor Gathering Description", DateTime.Now,
+            TimeSpan.FromHours(4), new Address("789 Oak St", "City", "State", "Country"), "Sunny");
+
+        
+        Console.WriteLine("Lecture Event:");
+        Console.WriteLine(lectureEvent.GetStandardDetails());
+        Console.WriteLine(lectureEvent.GetFullDetails());
+        Console.WriteLine(lectureEvent.GetShortDescription());
+        Console.WriteLine();
+
+        Console.WriteLine("Reception Event:");
+        Console.WriteLine(receptionEvent.GetStandardDetails());
+        Console.WriteLine(receptionEvent.GetFullDetails());
+        Console.WriteLine(receptionEvent.GetShortDescription());
+        Console.WriteLine();
+
+        Console.WriteLine("Outdoor Gathering Event:");
+        Console.WriteLine(outdoorGatheringEvent.GetStandardDetails());
+        Console.WriteLine(outdoorGatheringEvent.GetFullDetails());
+        Console.WriteLine(outdoorGatheringEvent.GetShortDescription());
+    }
+    }
+
+public class Event
+{
+    private string title;
+    private string description;
+    private DateTime date;
+    private TimeSpan time;
+    private Address address;
+
+    public Event(string title, string description, DateTime date, TimeSpan time, Address address)
+    {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+        this.address = address;
+    }
+
+    public string GetStandardDetails()
+    {
+        return $"Title: {title}\nDescription: {description}\nDate: {date.ToShortDateString()}\nTime: {time.ToString()}\nAddress: {address.ToString()}";
+    }
+
+    public virtual string GetFullDetails()
+    {
+        return GetStandardDetails();
+    }
+
+    public virtual string GetShortDescription()
+    {
+        return $"Type: Event\nTitle: {title}\nDate: {date.ToShortDateString()}";
+    }
+}
+
+
+public class LectureEvent : Event
+{
+    private string speaker;
+    private int capacity;
+
+    public LectureEvent(string title, string description, DateTime date, TimeSpan time, Address address, string speaker, int capacity)
+        : base(title, description, date, time, address)
+    {
+        this.speaker = speaker;
+        this.capacity = capacity;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{base.GetFullDetails()}\nType: Lecture\nSpeaker: {speaker}\nCapacity: {capacity}";
+    }
+}
+
+
+public class ReceptionEvent : Event
+{
+    private string rsvpEmail;
+
+    public ReceptionEvent(string title, string description, DateTime date, TimeSpan time, Address address, string rsvpEmail)
+        : base(title, description, date, time, address)
+    {
+        this.rsvpEmail = rsvpEmail;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{base.GetFullDetails()}\nType: Reception\nRSVP Email: {rsvpEmail}";
+    }
+}
+
+
+public class OutdoorGatheringEvent : Event
+{
+    private string weatherForecast;
+
+    public OutdoorGatheringEvent(string title, string description, DateTime date, TimeSpan time, Address address, string weatherForecast)
+        : base(title, description, date, time, address)
+    {
+        this.weatherForecast = weatherForecast;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{base.GetFullDetails()}\nType: Outdoor Gathering\nWeather Forecast: {weatherForecast}";
+    }
+}
+
+
+public class Address
+{
+    private string street;
+    private string city;
+    private string state;
+    private string country;
+
+    public Address(string street, string city, string state, string country)
+    {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+
+    public override string ToString()
+    {
+        return $"{street}, {city}, {state}, {country}";
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,126 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+         List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 3.0),
+            new Running(new DateTime(2022, 11, 3), 30, 4.8),
+            new Cycling(new DateTime(2022, 11, 3), 45, 15),
+            new Swimming(new DateTime(2022, 11, 3), 40, 10)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }
+}
+
+abstract class Activity
+{
+    protected DateTime date;
+    protected int minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        this.date = date;
+        this.minutes = minutes;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public string GetSummary()
+    {
+        double distance = GetDistance();
+        double speed = GetSpeed();
+        double pace = GetPace();
+
+        string summary = $"{date.ToString("dd MMM yyyy")} {GetType().Name} ({minutes} min) - ";
+        summary += $"Distance: {distance} miles, Speed: {speed} mph, Pace: {pace} min/mile";
+        return summary;
+    }
+}
+
+class Running : Activity
+{
+    private double distance;
+
+    public Running(DateTime date, int minutes, double distance)
+        : base(date, minutes)
+    {
+        this.distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (distance / minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return minutes / distance;
+    }
+}
+
+class Cycling : Activity
+{
+    private double speed;
+
+    public Cycling(DateTime date, int minutes, double speed)
+        : base(date, minutes)
+    {
+        this.speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return speed * (minutes / 60);
+    }
+
+    public override double GetSpeed()
+    {
+        return speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / speed;
+    }
+}
+
+class Swimming : Activity
+{
+    private int laps;
+
+    public Swimming(DateTime date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        this.laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return laps * 50 / 1000 * 0.62;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return minutes / GetDistance();
     }
 }
